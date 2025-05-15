2e8078d0f95cfa8a05965b26b97538dabdbf304a
Mario <oiramdeveloper@gmail.com>
2025-05-15 13:00:17 +0200

    actu

--- src/main/java/Fxapp/Practica3trimestre/FormLibro.java
+++ src/main/java/Fxapp/Practica3trimestre/FormLibro.java
@@ -1,17 +1,24 @@
 package Fxapp.Practica3trimestre;
 
+import java.io.File;
+
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class FormLibro extends GridPane {
 	public FormLibro() {
 		// Ponemos el espaciado
 		this.setVgap(10);
 		this.setHgap(10);
-		this.setPadding(new Insets(10));
+		this.setPadding(new Insets(20));
 
 		Label tituloLabel = new Label("Titulo");
 		TextField tituloField = new TextField();
@@ -31,6 +38,45 @@ public class FormLibro extends GridPane {
 		autorTextField.setPromptText("Autor/a");
 		this.add(autorLabel, 0, 3);
 		this.add(autorTextField, 1, 3);
+
+		Label anioPublLabel = new Label("Año de Publicacion");
+		Slider sldAnioPubl = new Slider();
+		sldAnioPubl.setShowTickLabels(true);
+		sldAnioPubl
+				.setOnMouseReleased(e -> anioPublLabel.setText("Año de Publicacion: " + (int) sldAnioPubl.getValue()));
+		sldAnioPubl.setMin(1800);
+		sldAnioPubl.setMax(2025);
+		this.add(anioPublLabel, 0, 4);
+		this.add(sldAnioPubl, 1, 4);
+
+		Label disponibleLabel = new Label("Disponibilidad");
+		CheckBox chxbDisponible = new CheckBox();
+		this.add(disponibleLabel, 0, 5);
+		this.add(chxbDisponible, 1, 5);
+
+		Stage fileStage = new Stage();
+		Button fileChooserBtn = new Button("Selecciona Archivo");
+
+		fileChooserBtn.setOnAction(event -> {
+			// Crear y configurar el FileChooser
+			FileChooser fileChooser = new FileChooser();
+			FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("Todos los archivos", "*.*");
+			fileChooser.getExtensionFilters().add(filter);
+
+			// Abrir el gestor de archivos
+			File archivoSeleccionado = fileChooser.showOpenDialog(fileStage);
+
+			// Verifica si se ha seleccionado un archivo
+			if (archivoSeleccionado != null) {
+				// Copiar la ruta del archivo a un String
+				String rutaArchivo = archivoSeleccionado.getAbsolutePath();
+				Label rutaArchivolbl = new Label();
+				rutaArchivolbl.setText("Ruta del archivo" + rutaArchivo);
+				this.add(rutaArchivolbl, 3, 6);
+
+			}
+		});
+		this.add(fileChooserBtn, 0, 6);
 
 	}
 }
